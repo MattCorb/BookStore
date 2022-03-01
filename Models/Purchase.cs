f3ebfd380885c3964c19ddf505f07ff02403186b
MattCorb <32273733+MattCorb@users.noreply.github.com>
2022-03-01 14:57:15 -0700

    purchases

---
+++ Models/Purchase.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Models
+{
+    public class Purchase
+    {
+
+
+        [Key]
+        [BindNever]
+        public int PurchaseId { get; set; }
+        [BindNever]
+        public ICollection<BasketLineItem> Lines { get; set; }
+        [Required(ErrorMessage ="Please Enter a Name")]
+        public string Name { get; set; }
+        [Required(ErrorMessage ="Please enter first Address")]
+        public string AddressLine1 { get; set; }
+        public string AddressLine2 { get; set; }
+        public string AddressLine3 { get; set; }
+        [Required(ErrorMessage = "Please enter a city")]
+        public string City { get; set; }
+        public string State { get; set; }
+
+        public string Zip { get; set; }
+        [Required(ErrorMessage = "Please enter a country")]
+        public string Country { get; set; }
+    }
+}
